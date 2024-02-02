9e600d19f7799dc043012709e7057edde952d4b7
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-02 14:29:40 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(8+8) // we are using nodejs, not browser
+
+let name = "Dev"            // String dataType
+let age = 22                // Integer dataType
+let isLoggedIn = true       // Boolean dataType
+
+
+// Notes :
+// number => Ranges from 2 to power of 53
+// bigint => Mostly you'll not use bigint but in some case if you've big numbers (bigint is used in big websites like Facebook, Reddit, Trading etc...)
+// String => You can use this in Single quotes or in Double quotes.
+// boolean => You can use (true/false) 
+// null => null is a standalone value.
+// undefined => undefined means you've not defined the values.
+// symbol => (unique) To find the uniqueness of Components we use symbols.
+
+
+// object 
+
+// To know the type of dataType =>
+console.log(typeof "Dev");
+console.log(typeof 22);
+console.log(typeof age);
+console.log(typeof null);  // null is a typeof object 
+console.log(typeof undefined); //It is a type 
