fd101310d87fb766778e2f0c79913061606ecb88
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-02 17:18:09 +0000

    CoversionOperation of datatypes

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -32,5 +32,41 @@ let booleanIsLoggedIn = Boolean (isLoggedIn)
 let someNumber = 88
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber); // Checking the typeof converion 
+// console.log(stringNumber);
+// console.log(typeof stringNumber);           // Checking the typeof converion 
+
+
+// ********************************* Operations ************************************
+
+let value = 8
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Dev"
+
+let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log((1 + 2)+ "2" );
+
+// console.log(true);
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
