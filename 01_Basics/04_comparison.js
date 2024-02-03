df8650fdfc036fb224cdf938b0188d4e478636c3
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-03 16:58:00 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,24 @@
+// Basic Comparison
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// WE AVOID THESE TYPE OF CONVERSION
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===
+console.log("2" === 2);
