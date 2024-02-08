50f626fede7fc0547ffb5c902c1c128d332c97d3
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-08 18:01:11 +0530

    Delete 01_Basics/05_Stack&HeapMemory.js

--- 01_Basics/05_Stack&HeapMemory.js
+++
@@ -1,6 +0,0 @@
-// Stack Memory (used in Primitive Type) & Heap Memory (used in Non-Primitive Type)
-
-// Whenever Stack memory is used like when you delclared a variable you'll get a copy
-// Whenever a memory is defined inside the Heap memory (like Object or anything) from there you get reference / it get change in Original value
-
-let myGithubname = "Dev"
