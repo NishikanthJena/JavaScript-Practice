6a44c69b24b9696c0a72d3421fd93da30b168836
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-09 17:30:54 +0000

    strings commit

--- 01_Basics/06_strings.js
+++ 01_Basics/06_strings.js
@@ -1,2 +1,9 @@
 
-console.log("hello " + "guys");
+// console.log("hello " + "guys");  // Basic of string 
+
+const name = "Nish"
+const id = 8
+
+// console.log(name + id + " Value");  // This is outdated one
+
+console.log(`Hello my name is ${name} and my id is ${id}`);
