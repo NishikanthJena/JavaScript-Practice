1232a16d623f9ff69d5595636dee4bd8197c47b4
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-13 17:21:08 +0000

    Rounding a number

--- Javascript/01_roundaNumber.js
+++ Javascript/01_roundaNumber.js
@@ -1,4 +1,6 @@
 // Rounding a number => Taking a number & moving it to the nearest number
 
 Math.round(2.2);
-console.log(Math.round(2.2));
+console.log(Math.round(2.2)); // Output -> 2
+
+console.log(Math.round(2.8)); // Output -> 3
