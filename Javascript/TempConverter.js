d9729facbcc002ca6d85472eedded463e837ccc2
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-14 14:52:19 +0000

    Convert temperature

---
+++ Javascript/TempConverter.js
@@ -0,0 +1,9 @@
+// Convert temperature from Celsius (°C) to Fahrenheit (°F) 
+// Formula : Celsius = (Fahrenheit - 32) * 5/9
+// Formula : Fahrenheit = (Celsius * 9/5) + 32
+
+
+// Temperature is 25°. Calcuate Temperature from Celsius (°C) to Fahrenheit (°F) 
+Fahrenheit = (25 * 9/5) + 32;
+console.log(Fahrenheit);
+
