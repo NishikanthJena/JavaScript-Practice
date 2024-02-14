41d67d9d9422ce9af84528cb02f93c5c1dc194c6
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-14 14:55:11 +0000

    Convert temperature

--- Javascript/TempConverter.js
+++ Javascript/TempConverter.js
@@ -3,7 +3,13 @@
 // Formula : Fahrenheit = (Celsius * 9/5) + 32
 
 
-// Temperature is 25°. Calcuate Temperature from Celsius (°C) to Fahrenheit (°F) 
+// Temperature is 25°C. Calcuate Temperature from Celsius (°C) to Fahrenheit (°F) 
 Fahrenheit = (25 * 9/5) + 32;
-console.log(Fahrenheit);
+console.log(Fahrenheit);    // Output : 77
+
+// Temperature is 86°F. Calcuate Temperature from Fahrenheit (°F) to Celsius (°C) 
+Celsius = (86 - 32) * 5/9
+console.log(Celsius);       // Output : 30
+
+
 
