325cb7b11fd0f860419a40640074b177f24ce18d
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-14 14:09:46 +0000

    round a number down

--- Javascript/01_roundaNumber.js
+++ Javascript/01_roundaNumber.js
@@ -4,3 +4,10 @@ Math.round(2.2);
 console.log(Math.round(2.2)); // Output -> 2
 
 console.log(Math.round(2.8)); // Output -> 3
+
+// Let's say we want to round a number down (2.8=>2) 
+
+let num = 2.8;
+let roundedDown = Math.floor(2.8);
+console.log(roundedDown);
+
