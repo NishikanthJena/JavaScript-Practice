04a6bfabd3ff189c550476f16977621c2b4721d2
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-15 16:07:49 +0000

    commit strings

---
+++ Javascript/02_strings.js
@@ -0,0 +1,19 @@
+// Strings = Text
+console.log('Hello');
+
+// Syntax of String : 'hello'
+// 'Hello' => Write a text hello & then surround this text with ''-Single quotes (one at Beginning & one at end)
+
+console.log('Some'+'Text'); // Output : 'SomeText'
+
+console.log(typeof 2);  // Number
+console.log(typeof Hello);  // String
+
+// Concatenation
+console.log('Items (' + (1+1) + ') : $' +(2095 + 799) / 100);
+
+// 3 ways to create a String
+
+// 
+
+
