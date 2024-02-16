bd666f903d3c57fca0b639a734766082680b1a51
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-16 17:19:17 +0000

    Commit String

--- 01_Basics/06_strings.js
+++ 01_Basics/06_strings.js
@@ -4,6 +4,11 @@
 const name = "Nish"
 const id = 8
 
+
+// Concatenation
+console.log('Items (' + (1+1) + ') : $' +(2095 + 799) / 100);
+
+
 // console.log(name + id + " Value");  // This is outdated one
 
 console.log(`Hello my name is ${name} and my id is ${id}`);
