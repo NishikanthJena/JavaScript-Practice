bcd9673d5e6b0491bfddc49444a848e70e99534a
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-17 16:52:08 +0000

    Commit Variables

---
+++ Javascript/03_variables.js
@@ -0,0 +1,27 @@
+//* ====================================
+//* Values and Variable in JavaScript
+//* ====================================
+
+//* In JavaScript, values and variables are fundamental concepts that form the basis of programming.
+
+//* Values:  A value is a piece of information that a program can work with. It can be a number, text, true/false, or more complex data.
+
+//* Variables: A variable is a container that holds a value. It has a name and can be used to store and manipulate data in a program.
+
+//! Let's test : Vaild or Not Valid
+
+// var my_firstName = `Dev`;       // This is a Vaild variable name
+// console.log(my_firstName);       // We can use underscore between the words
+
+// var _myLastName$ = `jen`; 
+// console.log(_myLastName$);       // This is a Vaild variable name
+
+// var 123myAge = 25;            //  Not Vaild : Bcoz Variable name cannot start with numeric value
+// console.log(123myAge);       //   Bcoz An identifier or keyword cannot immediately follow a numeric literal.
+
+// var $cityName = "New York";  
+// console.log($cityName);          // Vaild
+
+// var my@Email = "dev@me.com";        // Not Vaild : 
+// console.log(my@Email);                  
+
