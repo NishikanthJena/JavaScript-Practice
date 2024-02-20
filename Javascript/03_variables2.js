5a708abdc7b424045c8bd761bbae659652eb3486
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-20 17:22:33 +0000

    commit variables

--- Javascript/03_variables2.js
+++ Javascript/03_variables2.js
@@ -21,8 +21,11 @@ console.log(variable1);
 
 const variable2 = 8;
 
+// In order to keep the code safer -> 
 // Best practice = use const by default.
 // (Only use let when we need to change the variable)
+
+
 
 
 
