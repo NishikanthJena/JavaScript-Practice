2ad009f3346945678e66b45c5869de1932913904
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-20 17:42:25 +0000

    commit Naming Conventions

---
+++ Javascript/04_namingConventions.js
@@ -0,0 +1,10 @@
+// camelCase => cartQuantity
+// PascelCase => cartQuantity
+// -(same as camelcase)
+// - except we capitalize first WebTransportDatagramDuplexStream. 
+// - one feature where we use pascel case
+
+// Kebad csae => cart-quantity
+
+// Snake_case => 
+cart_quantity
