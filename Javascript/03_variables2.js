a307bac45405ce002930908a7c20f4692cec7c81
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-20 17:31:19 +0000

    commit variables

--- Javascript/03_variables2.js
+++ Javascript/03_variables2.js
@@ -1,7 +1,7 @@
 // 3 ways to create variables in Js
 
-// let
-// const
+// let => if we have to change a variable, then use let 
+// const => use const by 
 // var
 
 let variable1 = 3;      
@@ -17,17 +17,27 @@ variable1 = 5;     // Re-assigning the value to a variable
 console.log(variable1);
 
 
+// Const
+
 // Const => Can't change its value later. Value stays constant.
 
 const variable2 = 8;
 
-// In order to keep the code safer -> 
-// Best practice = use const by default.
+// In order to keep the code safer -> Best practice = use const by default.
 // (Only use let when we need to change the variable)
 
+const messgae = `hello`;
+console.log(messgae)
 
 
+// Var
 
+// var => var creates a variable just like let & This variable can change later.
+// var is actually the orginal way to create the variable in javascript.
+
+// var => Means variable
+
+// var have some issues bcoz of that issues we don't use var in new javascript code. 
 
 
 
