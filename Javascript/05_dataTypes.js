d73e12c8ffa0826211480dc0864473c410a24264
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-21 16:56:16 +0000

    commit datatypes

--- Javascript/05_dataTypes.js
+++ Javascript/05_dataTypes.js
@@ -6,7 +6,10 @@
 
 //? Types of Primitive Data types
 
-
+//? String: Represents a sequence of characters enclosed in single or double quotes.
+// Example:
+const myName = `Nish`;
+console.log(myName);
 
 
 //? Number: Represents numeric values, including integers and floating-point numbers.
