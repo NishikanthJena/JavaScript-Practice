703949146142df9399665a1917ffe1ecdebe54a5
NishiKanth Jena <141557880+NishikanthJena@users.noreply.github.com>
2024-02-21 16:55:52 +0000

    commit datatype

---
+++ Javascript/05_dataTypes.js
@@ -0,0 +1,17 @@
+//* ============================
+//* Data Types Section
+//* ============================
+
+//* Data types define the type of values that a variable can hold.
+
+//? Types of Primitive Data types
+
+
+
+
+//? Number: Represents numeric values, including integers and floating-point numbers.
+// Example:
+let myfavNum = 8;
+console.log(myfavNum);
+
+
